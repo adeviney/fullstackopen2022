ed86fb211ca29c8a72e8c9e9bc5415d25390863e
Alexis Deviney <alexis.deviney36@gmail.com>
2022-05-08 17:46:16 -0700

    ex 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -13,18 +13,32 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   const increaseValue = (category) => {
-    if (category == 'good') {
+    if (category === 'good') {
       console.log("good feedback")
       setGood(good + 1)
 
-    } else if (category == 'neutral') {
+    } else if (category === 'neutral') {
       console.log("neutral feedback")
       setNeutral(neutral + 1)
       
-    } else if (category == 'bad') {
+    } else if (category === 'bad') {
       console.log("bad feedback")
       setBad(bad + 1)
     }
+  }
+
+  const aggregateStats = () => {
+    const all = good + neutral + bad
+    const average = (good * 1 + bad * -1) / all
+    const posperc = good / all
+
+    return (
+      <div>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {posperc}%</p>
+      </div>
+    )
   }
   return (
     <div>
@@ -38,6 +52,10 @@ const App = () => {
     <p>good {good}</p>
     <p>neutral {neutral}</p>
     <p>bad {bad}</p>
+
+    {aggregateStats()}
+    
+    
     </div>
   )
 }
