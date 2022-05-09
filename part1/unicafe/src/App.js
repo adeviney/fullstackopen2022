d8c66fdabfc451d0b4da6a70cecda8a99f4d82a0
Alexis Deviney <alexis.deviney36@gmail.com>
2022-05-08 17:50:55 -0700

    ex 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,6 +6,23 @@ const Button = ({ handleClick, text }) => (
   </button>
 )
 
+const Statistics = ({good, neutral, bad}) => {
+  const all = good + neutral + bad
+  const average = (good * 1 + bad * -1) / all
+  const posperc = good / all
+
+  return (
+    <div>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {average}</p>
+      <p>positive {posperc}%</p>
+    </div>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -27,19 +44,6 @@ const App = () => {
     }
   }
 
-  const aggregateStats = () => {
-    const all = good + neutral + bad
-    const average = (good * 1 + bad * -1) / all
-    const posperc = good / all
-
-    return (
-      <div>
-      <p>all {all}</p>
-      <p>average {average}</p>
-      <p>positive {posperc}%</p>
-      </div>
-    )
-  }
   return (
     <div>
     <h1>give feedback</h1>
@@ -49,11 +53,7 @@ const App = () => {
     <Button handleClick = {() => increaseValue("bad")} text = "bad"/>
 
     <h1>statistics</h1>
-    <p>good {good}</p>
-    <p>neutral {neutral}</p>
-    <p>bad {bad}</p>
-
-    {aggregateStats()}
+    <Statistics good = {good} neutral = {neutral} bad = {bad}/>
     
     
     </div>
