9afd011fe7b2cb2b6ed654875fed007bca629c68
Alexis Deviney <alexis.deviney36@gmail.com>
2022-07-07 18:30:46 -0700

    exercise 1.6
    redoing this since it has been a while

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,46 +1,10 @@
 import { useState } from 'react'
 
-const Button = ({ handleClick, text }) => (
+const Button = ( {handleClick, text}) => (
   <button onClick={handleClick}>
-    {text}
+  {text}
   </button>
 )
-
-const StatisticLine = ({text, value}) => {
-  if (text === "positive") {
-    return(
-      <p>{text} {value}%</p>
-    )
-  }
-  return(
-    <p>{text} {value}</p>
-  )
-}
-
-const Statistics = ({good, neutral, bad}) => {
-  const all = good + neutral + bad
-  if (all === 0) {
-    return (
-      <div>
-        No feedback given
-      </div>
-    )
-  }
-  const average = (good * 1 + bad * -1) / all
-  const posperc = good / all
-
-  return (
-    <div>
-      <StatisticLine text = "good" value = {good} />
-      <StatisticLine text = "neutral" value = {neutral} />
-      <StatisticLine text = "bad" value = {bad} />
-      <StatisticLine text = "all" value = {all} />
-      <StatisticLine text = "average" value = {average} />
-      <StatisticLine text = "positive" value = {posperc} />
-
-    </div>
-  )
-}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -48,33 +12,18 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const increaseValue = (category) => {
-    if (category === 'good') {
-      console.log("good feedback")
-      setGood(good + 1)
-
-    } else if (category === 'neutral') {
-      console.log("neutral feedback")
-      setNeutral(neutral + 1)
-      
-    } else if (category === 'bad') {
-      console.log("bad feedback")
-      setBad(bad + 1)
-    }
-  }
 
   return (
     <div>
-    <h1>give feedback</h1>
+      <h1>give feedback</h1>
+      <Button handleClick = {() => setGood(good+1)} text = "good"/>
+      <Button handleClick = {() => setNeutral(neutral+1)} text = "neutral"/>
+      <Button handleClick = {() => setBad(bad+1)} text = "bad"/>
 
-    <Button handleClick = {() => increaseValue("good")} text = "good"/>
-    <Button handleClick = {() => increaseValue("neutral")} text = "neutral"/>
-    <Button handleClick = {() => increaseValue("bad")} text = "bad"/>
-
-    <h1>statistics</h1>
-    <Statistics good = {good} neutral = {neutral} bad = {bad}/>
-    
-    
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   )
 }
