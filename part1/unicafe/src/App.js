d27be1e90a6d49a64b17c0d5d72feeea0db1433e
Alexis Deviney <alexis.deviney36@gmail.com>
2022-07-08 19:12:03 -0700

    fixed warning in console by adding <tbody> tag <tbody>

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -38,12 +38,14 @@ const Statistics = ({good, neutral, bad}) => {
   return ( <>
   <h1>statistics</h1>
   <table>
-    <StatisticLine text="good" value = {good}/>
-    <StatisticLine text="neutral" value = {neutral}/>
-    <StatisticLine text="bad" value = {bad}/>
-    <StatisticLine text="all" value = {good+neutral+bad}/>
-    <StatisticLine text="average" value = {(good - bad)/(good+neutral+bad)}/>
-    <StatisticLine text="positive" value = {good/(good+neutral+bad)*100}/>
+    <tbody>
+      <StatisticLine text="good" value = {good}/>
+      <StatisticLine text="neutral" value = {neutral}/>
+      <StatisticLine text="bad" value = {bad}/>
+      <StatisticLine text="all" value = {good+neutral+bad}/>
+      <StatisticLine text="average" value = {(good - bad)/(good+neutral+bad)}/>
+      <StatisticLine text="positive" value = {good/(good+neutral+bad)*100}/>
+    </tbody>
   </table>
   </>
   )
