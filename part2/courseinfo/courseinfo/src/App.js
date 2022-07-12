085b580212907452d260ddd2a3a5d936cda16aed
Alexis Deviney <alexis.deviney36@gmail.com>
2022-07-11 18:04:13 -0700

    ex 2.5
    separate module

--- part2/courseinfo/courseinfo/src/App.js
+++ part2/courseinfo/courseinfo/src/App.js
@@ -1,31 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ sum }) => <p><b>total of {sum} exercises</b></p>
-
-const Part = ({ part }) =>
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) =>
-  <>
-    {parts.map(part =>
-      <Part key={part.id} part={part} />
-    )}
-  </>
-
-const Course = ({ course }) => {
-  const exerciseSumTotal = course.parts.reduce((sum, part) => sum + part.exercises, 0)
-
-  return (
-    <>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total sum={exerciseSumTotal} />
-    </>
-  )
-}
-
+import Course from './components/Course'
 
 const App = () => {
   const courses = [{
